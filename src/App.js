cc6fbfa1f7a013bf47f6816ea756f6398d330c4e
Cristian <cristian.echeverri4@gmail.com>
2020-05-29 05:51:05 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react"
+import TimeAgo from "./components/TimeAgo"
+import "./App.css"
 
 function App() {
+  const [time, setTime] = useState("")
+  const [data, setData] = useState({
+    time: "",
+    dynamic: false,
+    includeTime: false,
+  })
+  const dynamic = useRef(false)
+  const includeTime = useRef(false)
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setData({
+      time,
+      dynamic: dynamic.current.checked,
+      includeTime: includeTime.current.checked,
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+          placeholder="Enter time"
+        />
+        <label>
+          Dynamic:
+          <input type="checkbox" ref={dynamic} />
+        </label>
+        <label>
+          Include Time:
+          <input type="checkbox" ref={includeTime} />
+        </label>
+        <button className="btn" type="submit">
+          Submit
+        </button>
+      </form>
+      {data.time && (
+        <TimeAgo
+          targetTime={data.time}
+          dynamic={data.dynamic}
+          includeTime={data.includeTime}
+        />
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/TimeAgo.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react"
+import "../App.css"
+
+export default function TimeAgo(props) {
+  const [timeAgo, setTimeAgo] = useState("")
+  const { targetTime, includeTime, dynamic } = props
+
+  function countIncludeTime(date) {
+    const difference = +new Date() - +new Date(date)
+    let remaining = ""
+
+    if (difference > 0) {
+      const parts = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      }
+
+      remaining = Object.keys(parts)
+        .map((part) => {
+          if (!parts[part]) return
+          return `${parts[part]} ${part}`
+        })
+        .join(" ")
+    }
+
+    return remaining
+  }
+
+  useEffect(() => {
+    setTimeAgo(countIncludeTime(targetTime))
+  }, [])
+
+  useEffect(() => {
+    if (dynamic) {
+      setTimeAgo(countIncludeTime(targetTime))
+      const interval = setInterval(() => {
+        setTimeAgo(countIncludeTime(targetTime))
+      }, 1000 * 60)
+      return () => clearInterval(interval)
+    }
+  }, [targetTime, dynamic])
+
+  useEffect(() => {
+    if (includeTime) {
+      setTimeAgo(countIncludeTime(targetTime))
+      const interval = setInterval(() => {
+        setTimeAgo(countIncludeTime(targetTime))
+      }, 1000)
+      return () => clearInterval(interval)
+    }
+  }, [includeTime])
+
+  return <span>{timeAgo}</span>
+}
